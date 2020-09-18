b99b2f72266533fc1e3370b0e55cfec3b1e18173
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-18 18:55:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -68,7 +68,7 @@ def nyc_pigeon_organizer(data)
           new_hash[name] = {}
           new_hash[name][key] = []
         else
-          new_hash[name][key].push(inner_key)
+          new_hash[name][key].push(inner_key.to_s)
         end
       }
     }
