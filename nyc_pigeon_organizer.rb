9ea88c93814bf1d1c68326a0f5c5b9c53e2e911a
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-18 19:04:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -63,7 +63,7 @@ def nyc_pigeon_organizer(data)
   # binding.pry
   data.each_with_object({}) do |(key, value), new_hash|
     value.each { |inner_key, inner_values|
-      binding.pry
+      # binding.pry
       inner_values.each {|name|
         if !new_hash[name]
           new_hash[name] = {}
@@ -72,10 +72,10 @@ def nyc_pigeon_organizer(data)
           new_hash[name][key] = []
         end
         new_hash[name][key].push(inner_key.to_s)
-        binding.pry
+        # binding.pry
       }
     }
-    binding.pry
+    # binding.pry
     new_hash
   end
 
